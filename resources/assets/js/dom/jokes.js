37c377b7cc614e8171608d4f52fcc234ae2b1b18
manuelprg <manuel.contreras.morillo@gmail.com>
2018-03-16 00:57:36 +0100

    modificado jokes dom

--- resources/assets/js/dom/jokes.js
+++ resources/assets/js/dom/jokes.js
@@ -1,5 +1,8 @@
 const jokeServices = require('../services/jokes');
 
-jokeServices.getRandomJoke().then(function(data) {
-  console.log(data);
+jokeServices.getRandomJoke().then(function({joke, joke_image, owner}) {
+
+  $('#random-joke').html(joke);
+  $('#random-data').find('img').eq(0).attr('src', joke_image);
+  $('#random-data').find('span').eq(0).html(owner);
 });
