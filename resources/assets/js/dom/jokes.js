0149179c26444810bb118e5d9746045ae677d93d
manuelprg <manuel.contreras.morillo@gmail.com>
2018-03-16 02:30:39 +0100

    arreglado bug de número de apariciones

--- resources/assets/js/dom/jokes.js
+++ resources/assets/js/dom/jokes.js
@@ -1,8 +1,21 @@
 const jokeServices = require('../services/jokes');
 
-jokeServices.getRandomJoke().then(function({joke, joke_image, owner}) {
+jokeServices.getRandomJoke().then(function({joke, joke_image, owner, slug, isVisited}) {
 
   $('#random-joke').html(joke);
   $('#random-data').find('img').eq(0).attr('src', joke_image);
   $('#random-data').find('span').eq(0).html(owner);
+
+  isVisited++
+  
+  const classType = (isVisited == 1)? 'panel-success' : 'panel-primary';
+  const message = (isVisited == 1)? 'Bravo!! es la primera aparición de este chiste' : `El chiste ha aparecido ${isVisited} veces`;
+    
+  $('#random-panel').addClass(classType);
+  $('.panel-body').eq(0).html(message);
+
+  return {slug, isVisited};
+
+}).then(function({slug, isVisited}) {
+  jokeServices.markJokeAsVisited(slug, isVisited);
 });
